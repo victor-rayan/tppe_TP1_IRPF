a633fe9bf19b77d8c44940343629fb5b303bd2d4
Victor Rayan Ferreira <victor.rayan378@gmail.com>
2022-12-13 20:30:04 -0300

    triangulação rendimentos
    Co-Authored-By: Antonio Igor Carvalho <78360676+AntonioIgorCarvalho@users.noreply.github.com>
    Co-Authored-By: Wesley Santos <54296715+wesleysantos00@users.noreply.github.com>
    Co-Authored-By: Mateus Moreira <49012681+mateus-lm@users.noreply.github.com>

--- simulador_irpf/tests/test_cadastro_rendimento.py
+++ simulador_irpf/tests/test_cadastro_rendimento.py
@@ -22,5 +22,20 @@ def testCadastroDoisRendimentos():
 
     assert resultado == valor1 + valor2
 
+def testCadastroTresRendimentos():
+    simuladorIRPF = CadastroDeRendimentos()
+    valor1:float = 1000.5
+    valor2:float = 2550.2
+    valor3:float = 5000.7
+
+    simuladorIRPF.cadastrarRendimento("Salario", valor1)
+    simuladorIRPF.cadastrarRendimento("Aluguel", valor2)
+    simuladorIRPF.cadastrarRendimento("Heranca", valor3)
+
+
+    resultado = simuladorIRPF.getTotalRendimentos()
+
+    assert resultado == valor1 + valor2 + valor3
+
 
    
