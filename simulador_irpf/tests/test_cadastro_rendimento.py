76bb37df1e09690842f8805d5bac4cc5c6693834
Victor Rayan Ferreira <victor.rayan378@gmail.com>
2022-12-13 20:44:52 -0300

    parametrização rendimentos
    Co-Authored-By: Antonio Igor Carvalho <78360676+AntonioIgorCarvalho@users.noreply.github.com>
    Co-Authored-By: Wesley Santos <54296715+wesleysantos00@users.noreply.github.com>
    Co-Authored-By: Mateus Moreira <49012681+mateus-lm@users.noreply.github.com>

--- simulador_irpf/tests/test_cadastro_rendimento.py
+++ simulador_irpf/tests/test_cadastro_rendimento.py
@@ -1,41 +1,19 @@
+import pytest
 from codigo.cadastro_de_rendimentos import CadastroDeRendimentos
    
-def testCadastroUmRendimento():
+@pytest.mark.parametrize("descricao1,descricao2,descricao3,valor1,valor2,valor3",
+    [
+        ("Aluguel", "Dividendo", "Salario", 5550.59, 4556.71, 8894.0), 
+        ("Rendimento", "RendaFixa", "Acoes", 12456.52, 89.73, 86448.77), 
+        ("Previdencia", "Venda", "Dividendo", 12.51, 1235.7, 43627.2)
+    ])
+def testCadastroRendimento(descricao1: str, descricao2: str, descricao3: str, valor1: float, valor2: float, valor3: float):
     simuladorIRPF = CadastroDeRendimentos()
-    valor1: float = 1000
-    descricao = "Salario"
 
-    simuladorIRPF.cadastrarRendimento(descricao, valor1)
-
-    assert valor1 == simuladorIRPF.getTotalRendimentos()
-
-    
-def testCadastroDoisRendimentos():
-    simuladorIRPF = CadastroDeRendimentos()
-    valor1:float = 1000.5
-    valor2:float = 2550.2
-
-    simuladorIRPF.cadastrarRendimento("Salario", valor1)
-    simuladorIRPF.cadastrarRendimento("Aluguel", valor2)
-
-    resultado = simuladorIRPF.getTotalRendimentos()
-
-    assert resultado == valor1 + valor2
-
-def testCadastroTresRendimentos():
-    simuladorIRPF = CadastroDeRendimentos()
-    valor1:float = 1000.5
-    valor2:float = 2550.2
-    valor3:float = 5000.7
-
-    simuladorIRPF.cadastrarRendimento("Salario", valor1)
-    simuladorIRPF.cadastrarRendimento("Aluguel", valor2)
-    simuladorIRPF.cadastrarRendimento("Heranca", valor3)
-
+    simuladorIRPF.cadastrarRendimento(descricao1, valor1)
+    simuladorIRPF.cadastrarRendimento(descricao2, valor2)
+    simuladorIRPF.cadastrarRendimento(descricao3, valor3)
 
     resultado = simuladorIRPF.getTotalRendimentos()
 
     assert resultado == valor1 + valor2 + valor3
-
-
-   
