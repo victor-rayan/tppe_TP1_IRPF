7ac5317f5c183e47007d6c7b8486f34c34b9d465
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2022-12-18 22:11:12 -0300

    Duplicação

--- simulador_irpf/main/imposto_faixa_impostos.py
+++ simulador_irpf/main/imposto_faixa_impostos.py
@@ -1,33 +1,68 @@
 class ImpostoFaixaImpostos:
     def __init__(self, rendimentosTributaveis: float) -> None:
-        self.rendimentosTributaveis = rendimentosTributaveis
+        self._rendimentosTributaveis = rendimentosTributaveis
+        self._maximoFaixa1: float = 1903.98
+        self._maximoFaixa2: float = 922.67
+        self._maximoFaixa3: float = 924.40
+        self._maximoFaixa4: float = 913.63
+        self._maximoFaixa5: float = 5335.32
+        self._impostoFaixa1: float = 0
+        self._impostoFaixa2: float = 0.075
+        self._impostoFaixa3: float = 0.15
+        self._impostoFaixa4: float = 0.2250
+        self._impostoFaixa5: float = 0.2750
 
     def valorFaixa1(self) -> float:
-        return 1903.98
+        if(self._rendimentosTributaveis <= self._maximoFaixa1):
+            return self._rendimentosTributaveis
+        else:
+            return self._maximoFaixa1
 
     def valorFaixa2(self) -> float:
-        return 922.67
+        faixasAnteriores = self._maximoFaixa1
+        if(self._rendimentosTributaveis > faixasAnteriores + self._maximoFaixa2):
+            return self._maximoFaixa2
+        elif(self._rendimentosTributaveis <= faixasAnteriores):
+            return 0
+        else:
+            return self._rendimentosTributaveis - faixasAnteriores
 
     def valorFaixa3(self) -> float:
-        return 924.40
+        faixasAnteriores = self._maximoFaixa1 + self._maximoFaixa2
+        if(self._rendimentosTributaveis > faixasAnteriores + self._maximoFaixa3):
+            return self._maximoFaixa3
+        elif(self._rendimentosTributaveis <= faixasAnteriores):
+            return 0
+        else:
+            return self._rendimentosTributaveis - faixasAnteriores
 
     def valorFaixa4(self) -> float:
-        return 913.63
+        faixasAnteriores = self._maximoFaixa1 + self._maximoFaixa2 + self._maximoFaixa3
+        if(self._rendimentosTributaveis > faixasAnteriores + self._maximoFaixa4):
+            return self._maximoFaixa4
+        elif(self._rendimentosTributaveis <= faixasAnteriores):
+            return 0
+        else:
+            return self._rendimentosTributaveis - faixasAnteriores
 
     def valorFaixa5(self) -> float:
-        return 5335.32
+        faixasAnteriores = self._maximoFaixa1 + self._maximoFaixa2 + self._maximoFaixa3 + self._maximoFaixa4
+        if(self._rendimentosTributaveis <= faixasAnteriores):
+            return 0
+        else:
+            return self._rendimentosTributaveis - faixasAnteriores
 
     def valorImpostoFaixa1(self) -> float:
-        return 0
+        return self.valorFaixa1() * self._impostoFaixa1
 
     def valorImpostoFaixa2(self) -> float:
-        return 69.2003
+        return self.valorFaixa2() * self._impostoFaixa2
 
     def valorImpostoFaixa3(self) -> float:
-        return 138.6600
+        return self.valorFaixa3() * self._impostoFaixa3
 
     def valorImpostoFaixa4(self) -> float:
-        return 205.5667
+        return self.valorFaixa4() * self._impostoFaixa4
 
     def valorImpostoFaixa5(self) -> float:
-        return 1467.2130
+        return self.valorFaixa5() * self._impostoFaixa5
--- simulador_irpf/tests/test_imposto_faixa_impostos.py
+++ simulador_irpf/tests/test_imposto_faixa_impostos.py
@@ -1,18 +1,35 @@
 import pytest
+from decimal import *
 from main.imposto_faixa_impostos import ImpostoFaixaImpostos
 
-simulador = ImpostoFaixaImpostos(10000)
+def testCalculaValorFaixaUm():
+    simulador = ImpostoFaixaImpostos(1500)
+    assert 1500 == simulador.valorFaixa1()
+    assert 0 == simulador.valorFaixa2()
+    assert 0 == simulador.valorFaixa3()
+    assert 0 == simulador.valorFaixa4()
+    assert 0 == simulador.valorFaixa5()
 
-def testCalculaValorFaixa():
+def testCalculaValorFaixaCinco():
+    simulador = ImpostoFaixaImpostos(10000)
     assert 1903.98 == simulador.valorFaixa1()
     assert 922.67 == simulador.valorFaixa2()
     assert 924.40 == simulador.valorFaixa3()
     assert 913.63 == simulador.valorFaixa4()
     assert 5335.32 == simulador.valorFaixa5()
 
-def testCalculaValorImpostoFaixa():
-    assert 0 == simulador.valorImpostoFaixa1()
-    assert 69.2003 == simulador.valorImpostoFaixa2()
-    assert 138.6600 == simulador.valorImpostoFaixa3()
-    assert 205.5667 == simulador.valorImpostoFaixa4()
-    assert 1467.2130 == simulador.valorImpostoFaixa5()
+def testCalculaValorImpostoFaixaUm():
+    simulador = ImpostoFaixaImpostos(1500)
+    assert 0 == pytest.approx(simulador.valorImpostoFaixa1(), 0.001)
+    assert 0 == pytest.approx(simulador.valorImpostoFaixa2(), 0.001)
+    assert 0 == pytest.approx(simulador.valorImpostoFaixa3(), 0.001)
+    assert 0 == pytest.approx(simulador.valorImpostoFaixa4(), 0.001)
+    assert 0 == pytest.approx(simulador.valorImpostoFaixa5(), 0.001)
+
+def testCalculaValorImpostoFaixaCinco():
+    simulador = ImpostoFaixaImpostos(10000)
+    assert 0 == pytest.approx(simulador.valorImpostoFaixa1(), 0.001)
+    assert 69.2003 == pytest.approx(simulador.valorImpostoFaixa2(), 0.001)
+    assert 138.6600 == pytest.approx(simulador.valorImpostoFaixa3(), 0.001)
+    assert 205.5667 == pytest.approx(simulador.valorImpostoFaixa4(), 0.001)
+    assert 1467.2130 == pytest.approx(simulador.valorImpostoFaixa5(), 0.001)
