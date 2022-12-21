8fae7f817d0751d46dda2a824d879ce235e44b82
mateus-lm <mateuuusss@gmail.com>
2022-12-21 09:39:11 -0300

    Adicionado triangulação com parametrização Aliquota Efetiva

--- simulador_irpf/tests/test_aliquota_efetiva.py
+++ simulador_irpf/tests/test_aliquota_efetiva.py
@@ -1,11 +1,12 @@
 import pytest
 from main.aliquota_efetiva import AliquotaEfetiva
 
-def testVerificaAliquotaEfetiva():
-    assert 24.42 == AliquotaEfetiva(30000, 189.59).valorAliquotaEfetiva()
-    assert 23.76 == AliquotaEfetiva(27500, 568.77).valorAliquotaEfetiva()
 
-def testVerificaAliquotaEfetiva1():
-    assert 4.86 == AliquotaEfetiva(3500, 0).valorAliquotaEfetiva()
-    assert 25.01 == AliquotaEfetiva(35000, 0).valorAliquotaEfetiva()
-    assert 0.00 == AliquotaEfetiva(3500, 3791.80).valorAliquotaEfetiva()
+@pytest.mark.parametrize("totalRendimentos, totalDeducoes, aliquotaEsperada", 
+    [
+        (30000, 189.59, 24.42), 
+        (27500, 568.77, 23.76), 
+        (3500, 0, 4.86)
+    ])
+def testVerificaAliquotaEfetiva(totalRendimentos:float, totalDeducoes:float, aliquotaEsperada:float):
+    assert aliquotaEsperada == AliquotaEfetiva(totalRendimentos, totalDeducoes).valorAliquotaEfetiva()
